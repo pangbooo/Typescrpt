b2749f6e739988523e392f7e424bcf745b1c0f54
pangbo <dhc-bo.pang@dhc.com.cn>
2019-11-05 17:07:30 +0800

    Class(3-6)

---
+++ Js 基础/Es6/Class/3.实例属性的新写法.js
@@ -0,0 +1,38 @@
+class IncreasingCounter {
+    constructor() {
+      this._count = 0;
+    }
+    get value() {
+      console.log('Getting the current value!');
+      return this._count;
+    }
+    increment() {
+      this._count++;
+    }
+  }
+
+  class IncreasingCounter {
+    _count = 0;
+
+    constructor() {
+    }
+    get value() {
+      console.log('Getting the current value!');
+      return this._count;
+    }
+    increment() {
+      this._count++;
+    }
+  }
+
+  //静态属性(定义在类上的属性，区别实例的属性)
+  //old
+  class Foo{}
+  Foo.prop = 1
+
+
+  //(提案 )
+  class Foo{
+      static prop = 1;
+  }
+
