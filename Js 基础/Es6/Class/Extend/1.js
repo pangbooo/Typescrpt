484d25755e794c0eee103f407fd9562b75bced69
pangbo <1838529223@qq.com>
2020-07-11 12:48:02 +0800

    Es6 class 继承

--- Js 基础/Es6/Class/Extend/1.js
+++ Js 基础/Es6/Class/Extend/1.js
@@ -6,7 +6,7 @@ class Point{
 
 class ColorPoint extends Point{
     constructor(x,y,color) {
-        super(x,y); //调用父类constructor(x,y)
+        super(x,y); //调用父类constructor(x,y),新建父类的this对象。
         this.color = color
     }
 
@@ -15,12 +15,31 @@ class ColorPoint extends Point{
     }
 }
 
-//子类必须在constructor方法中调用super方法，否则新建实例时会报错
-//ES5 ==> 实质是先创造子类的实例对象this，然后再将父类的方法添加到this上面（Parent.apply(this)）。
-//ES6 ==> 先将父类实例对象的属性和方法，加到this上面（所以必须先调用super方法），然后再用子类的构造函数修改this。
+/**
+ * 
+ * 1.原理
+ * 
+ * 子类必须在constructor方法中调用super方法，否则新建实例时会报错
+ * ES5 ==> 实质是先创造子类的实例对象this，然后再将父类的方法添加到this上面（Parent.apply(this)）。
+ * ES6 ==> 先将父类实例对象的属性和方法，加到this上面（所以必须先调用super方法），然后再用子类的构造函数修改this。
+ * 
+ **/
 
+/**
+ * 
+ * 2.constructor方法会被默认添加
+ * 
+ **/
+class ColorPoint extends Point {
+}
 
+// 等同于
+class ColorPoint extends Point {
+  constructor(...args) {
+    super(...args);
+  }
+}
 
-//2.静态方法也会被继承
+// ==> 3.静态方法也会被继承
 var col = new ColorPoint(1,2,'red');
 col.hello() //hello
--- Js 基础/Es6/Class/Extend/2.super.js
+++ Js 基础/Es6/Class/Extend/2.super.js
@@ -1,18 +1,32 @@
-//1.super作为函数调用时，代表父类的构造函数 （不定义报错）
+/**
+ * 
+ * 1.super作为函数调用时，代表父类的构造函数 （不定义报错）
+ *   super作为函数调用时，只能用在子类的构造函数之中，用在其他地方就会报错。
+ * 
+ **/
+
 class A {}
 
 class B extends A {
   constructor() {
+    //super()指得是父类构造函数，但是返回B实例
+    // 等价于 A.prototype.constructor.call(this)。
     super();
+
   }
 }
 
 //等于
 class B extends A{}
 
+/**
+ * 
+ * 2.super作为对象时，在普通方法中，指向父类的原型对象(父类实例的属性和方法不能调用)；
+ *                  在静态方法中，指向父类。 
+ * 
+ **/
 
-//2.super作为对象时，在普通方法中，指向父类的原型对象(父类实例的属性和方法不能调用)；在静态方法中，指向父类。 
-
+ // ==> 定义在父类实例对象的属性是无法通过super对象调用的
 class A{
     constructor(){
         this.a = 1;
@@ -34,13 +48,11 @@ class B extends A {
 }
 
 let b = new B()
-// super.p() === A.prototype.p() // true
+super.p() === A.prototype.p() // true
 
 b.a //undefined
-// 因为a属性定义在父类A实例上，不再原型上
 
-
-//ES6 规定，在子类普通方法中通过super调用父类的方法时，方法内部的【this指向】当前的子类实例。 
+// ==> ES6 规定，在子类普通方法中通过super调用父类的方法时，方法内部的【this指向】当前的子类实例。 
 class A {
     constructor(){
         this.x = 1;
@@ -58,14 +70,14 @@ class B extends A{
 
     m(){
         super.print();
-        // === super.print.call(this)
+        // 实际上执行的是 -> super.print.call(this)
     }
 }
 
 let b = new B();
 b.m(); //2 
 
-//由于this指向子类实例，所以如果通过super对某个属性赋值，这时super就是this，赋值的属性会变成子类实例的属性。
+// ==> *** 由于this指向子类实例，所以如果通过super对某个属性赋值，这时super就是this，赋值的属性会变成子类实例的属性。
 class A{
     constructor(){
         this.x = 1
@@ -82,7 +94,7 @@ class B extends A{
     }
 }
 
-//super作为对象，用在【静态方法】之这时super将指向父类，而不是父类的原型对象。
+// ==> super作为对象，用在【静态方法】的这时super将指向父类，而不是父类的原型对象。
 class Parent {
     static myMethod(msg) {
       console.log('static', msg);
